8a10004a999144b0c2f74b0b65f0e03bfcc12308
Shehreyar Ali <shehreyar1ali@gmail.com>
2024-10-11 00:38:25 +0500

    fixed typo in prompt

--- prompts/zero_shot.py
+++ prompts/zero_shot.py
@@ -1,5 +1,5 @@
 ZERO_SHOT_PROMPT = """
-Given the following topic {input}, generate a quiz with a randon number of questions between 1 to 5.
+Given the following topic {input}, generate a quiz with 3 questions.
 
 {format_instructions}
 """
